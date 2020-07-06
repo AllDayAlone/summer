66209789fe4926855795e7cdd752abf49bdcd700
Egor <elarayjs@gmail.com>
2020-07-06 15:07:56 +0300

    feat: add traverse function

---
+++ src/traverseFunc.js
@@ -0,0 +1,18 @@
+var _ = require('lodash');
+
+export const traverse = (obj, func) => {
+    _.forIn(obj, (val, key) => {
+        obj[key] = func(val);
+        if (_.isArray(val)) {
+            val.forEach(function(el) {
+                if (_.isObject(el)) {
+                    traverse(el, func);
+                }
+            });
+        }
+        if (_.isObject(key)) {
+            traverse(obj[key], func);
+        }
+    });
+    return obj;
+}
