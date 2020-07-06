0ebdec27b4059564bcf85305bc43c9fc5d1c88ed
Anna Volodkova <kvetka99@mail.ru>
2020-07-06 15:44:02 +0300

    My map function

---
+++ src/map.js
@@ -0,0 +1,24 @@
+function myMapArray(obj, f) {
+    let arr = [];
+    obj.forEach((item, index, array) => arr.push(f(item)));
+    return arr;
+}
+
+function myMapObject(obj, f) {
+    let o = {};
+    let arr = [];
+    for (let key in obj) {
+        if(obj.hasOwnProperty(key))
+            arr.push(f(obj[key]));
+    }
+    return arr;
+}
+
+function map(obj, f) {
+    if (typeof obj === Array)
+        return myMapArray(obj, f);
+    else return myMapObject(obj, f);
+
+}
+
+module.exports = map;
