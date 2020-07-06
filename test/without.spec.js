05165b2ab9b13f14de23110e24077a70f5a11bc5
dzinrai <alexex.malkov@gmail.com>
2020-07-06 15:48:07 +0300

    Add tests

---
+++ test/without.spec.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const without = require('../src/without');
+
+function test() {
+  const array = [1, 2, 3, 4];
+
+  // success
+  assert.deepStrictEqual(without(array, 1), [2, 3, 4]);
+  assert.deepStrictEqual(without(array, 1, 2, 3, 4), []);
+  // no values
+  assert.deepStrictEqual(without(array), array);
+  // empty array
+  assert.deepStrictEqual(without([], 1, 2, 3), []);
+
+  console.log('Success!');
+}
+
+test();
