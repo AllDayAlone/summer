8756a7b2d5cd198a60447d58148ca1518af829fa
Alexey Goremykin <alexgaga1996@gmail.com>
2020-07-06 16:59:04 +0300

    Add validation

--- src/times.js
+++ src/times.js
@@ -2,6 +2,9 @@ const randomBool = () => Math.random() > 0.5;
 let quantity;
 
 const times = (randomBool, quantity) => {
+    if (typeof(quantity) !== number && quantity < 1){
+        return 'Error';
+    }
     let result = [];
     for (let i = 0; i < quantity; i++){
         result.push(randomBool());
