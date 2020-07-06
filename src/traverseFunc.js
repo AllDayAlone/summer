cae9326ed30ae89bdbc7184ea72635005ebb8b3e
Egor <elarayjs@gmail.com>
2020-07-06 16:24:12 +0300

    refactor: changed array check

--- src/traverseFunc.js
+++ src/traverseFunc.js
@@ -3,7 +3,7 @@ var _ = require('lodash');
 export const traverse = (obj, func) => {
     _.forIn(obj, (val, key) => {
         obj[key] = func(val);
-        if (_.isArray(val)) {
+        if (Array.isArray(val)) {
             val.forEach(function(el) {
                 if (_.isObject(el)) {
                     traverse(el, func);
