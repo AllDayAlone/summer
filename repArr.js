9ded82f538baf4f025653d7f57a8f8a6298c66f7
Loi Gloria <loigloria2001@gmail.com>
2020-07-07 23:00:31 +0300

    add spaces for readability, setup prettier

--- repArr.js
+++ repArr.js
@@ -1,8 +1,8 @@
-const returnDuplicate=(arr)=>{
-    let obj={};
-    for(let i=0; i<arr.length; i++){
-        if(obj[arr[i]]) return arr[i];
-        obj[arr[i]]=true;
+const returnDuplicate = (arr) => {
+    let obj = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (obj[arr[i]]) return arr[i];
+        obj[arr[i]] = true;
     }
     return null;
 }
